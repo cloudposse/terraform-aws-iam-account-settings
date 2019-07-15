3403ea3e2ec7e2b909dcc8435fe9d8ce5c6c9d40
Josh Myers <joshuajmyers@gmail.com>
2019-07-15 19:41:30 +0100

    Output min password length

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "account_alias" {
   value       = "${local.account_alias}"
 }
 
+output "minimum_password_length" {
+  description = "IAM account minimum password length"
+  value       = "${join("", aws_iam_account_password_policy.default.*.minimum_password_length)}"
+}
+
 output "signin_url" {
   description = "IAM users sign-in URL"
   value       = "https://${local.account_alias}.signin.aws.amazon.com/console"
