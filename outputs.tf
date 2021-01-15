1cb297718c0e1412c5e8595c6e6a6cb330b556ce
Matt Calhoun <matt@mattcalhoun.com>
2021-01-15 10:59:39 -0500

    update syntax

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "account_alias" {
   description = "IAM account alias"
-  value       = "${local.account_alias}"
+  value       = local.account_alias
 }
 
 output "minimum_password_length" {
   description = "IAM account minimum password length"
-  value       = "${join("", aws_iam_account_password_policy.default.*.minimum_password_length)}"
+  value       = join("", aws_iam_account_password_policy.default.*.minimum_password_length)
 }
 
 output "signin_url" {
