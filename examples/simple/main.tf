1a1e308c2169b0368d8310fbc5bac6267ff7f82f
Max Lobur <max_lobur@outlook.com>
2023-05-17 13:12:13 +0300

    Sync github

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -20,9 +20,9 @@ module "account_settings" {
 }
 
 output "account_alias" {
-  value = "${module.account_settings.account_alias}"
+  value = module.account_settings.account_alias
 }
 
 output "signin_url" {
-  value = "${module.account_settings.signin_url}"
+  value = module.account_settings.signin_url
 }
